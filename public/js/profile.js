58c269cac6e9dc62b081d21c49b7d4212891a84e
Mohit <ponnamohit367@gmail.com>
2020-01-16 10:35:38 +0530

    add intrests

---
+++ public/js/profile.js
@@ -0,0 +1,16 @@
+function add(type) {
+
+	
+	var element = document.createElement("input");
+
+	
+	element.setAttribute("type", "text");
+
+
+
+	var foo = document.getElementById("fooBar");
+ element.style.display= "block"
+	
+	foo.appendChild(element);
+
+}
