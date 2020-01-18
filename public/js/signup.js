b0f896edbbdf82e86aae4e84681da4332c98a52d
Chaitanya <ckulkarni120@gmail.com>
2020-01-19 03:07:03 +0530

    updates

--- public/js/signup.js
+++ public/js/signup.js
@@ -7,24 +7,59 @@ $(document).ready(function () {
     $('#cfpassword').on('keyup', function () {
         var password = $('#password').val();
         var cfpassword = $('#cfpassword').val();
-        if(cfpassword == password){
+        if (cfpassword == password) {
             $('#button').attr('disabled', false);
         }
-        else{
+        else {
             $('#button').attr('disabled', true);
         }
-        
+
     })
 
     $('#password').on('keyup', function () {
         var password = $('#password').val();
         var cfpassword = $('#cfpassword').val();
-        if(cfpassword == password){
+        if (cfpassword == password) {
             $('#button').attr('disabled', false);
         }
-        else{
+        else {
             $('#button').attr('disabled', true);
         }
-        
+
     })
+
+    $('#password').focus(function () {
+        $(this).popover({ content: "Password should be atleast 8 characters and should contain Uppercase characters (A-Z), Lowercase characters (a-z), Digits (0-9), Special characters (~!@#$%^&*_-+=`|\(){}[]:;'<>,.?/)" })
+        $(this).blur(function () {
+            $(this).popover('hide')
+        })
+    });
+
+    $('#stateId').hide()
+    $('#cityId').hide()
+
+    $('#countryId').click( function () {
+        
+        if ($('#countryId').val() != "") {
+            $('#stateId').show();
+        }
+        else {
+            $('#stateId').hide();            
+        }
+    })
+
+    $('#stateId').click(function () {
+        console.log($('#stateId').val())
+        if ($('#stateId').val() != "" ) {
+            $('#cityId').show();
+        }
+        else {
+           $('#cityId').hide();
+        }
+
+
+    })
+
+
+
 });
